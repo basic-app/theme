6a6818ad51412b54d563d3d8559ec48ce3be6eeb
basic-app <dev@basic-app.com>
2020-02-06 14:02:22 +0300

    n/a

--- Table.php
+++ Table.php
@@ -8,9 +8,8 @@ namespace BasicApp\Theme;
 
 use PhpTheme\Core\Theme;
 use PhpTheme\Core\HtmlHelper;
-use PhpTheme\Html\BaseTable;
 
-class Table extends BaseTable
+class Table extends \PhpTheme\Table\Table
 {
 
     const COLUMN = TableColumn::class;
