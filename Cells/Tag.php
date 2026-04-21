2d3c87514b12df7769dccf4a4f9e43668ae1312a
basic-app <dev@basic-app.com>
2026-04-21 18:58:52 +0300

    n/a

---
+++ Cells/Tag.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Basic App Dev Team
+ * @license MIT
+ */
+namespace BasicApp\SiteTheme\Cells;
+
+use CodeIgniter\View\Cells\Cell;
+
+class Tag extends Cell
+{
+    protected string $view = VENDORPATH . 'basic-app/theme/templates/tag.php';
+
+    public $tag;
+
+    public $attributes = [];
+
+    public $slot;
+}
---
+++ templates/tag.php
@@ -0,0 +1,1 @@
+<<?= $tag . stringify_attributes($attributes);?>><?= $slot;?></<?= $tag;?>>
